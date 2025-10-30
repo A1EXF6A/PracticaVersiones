0ca8bcc74fe84ff536c53a3eb7ff5e4b263ad1d4
A1EXF6A <aguachi4414@uta.edu.ec>
2025-10-29 22:42:59 -0500

    cambios en main

--- src/cuartouta/Main.java
+++ src/cuartouta/Main.java
@@ -11,10 +11,6 @@ package cuartouta;
 public class Main {
     public static void main(String[] args) {
         Login log = new Login();
-        log.setVisible(true);
-        
-        //dd
-        //p ff dd
-        //hola dd
+        log.setVisible(true);  
     }
 }
