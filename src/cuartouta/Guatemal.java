5ba6baf4ccad4ad8eeb5e7dfc4ee0f1aed3e68ed
stalin <t908guatemakstalyn@gmail.com>
2025-10-30 17:46:43 -0500

    guatemal

--- src/cuartouta/Guatemal.java
+++ src/cuartouta/Guatemal.java
@@ -9,6 +9,6 @@ package cuartouta;
  * @author Bryan S. Guatemal
  */
 public class Guatemal {
-   // hola esta es  mi archivo creado desde netbeanas y subido al sistema hlllaaa ffff
+   // hola esta es  mi archivo creado desde netbeanas y subido al sistema hlllaaa ffff ffff
     
 }
