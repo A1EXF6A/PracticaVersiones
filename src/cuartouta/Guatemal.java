84a8d8d9c0b4882fe61accf36bb603764987316e
stalin <t908guatemakstalyn@gmail.com>
2025-10-30 17:41:43 -0500

    El ingenierome pide  crear un archivo con mi apellido , soy Guatemal Bryan

---
+++ src/cuartouta/Guatemal.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cuartouta;
+
+/**
+ *
+ * @author Bryan S. Guatemal
+ */
+public class Guatemal {
+   // hola esta es  mi archivo creado desde netbeanas y subido al sistema
+}
